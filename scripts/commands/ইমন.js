4cb25ea8cb7bb024b478de9fd60a56308e418f25
X-Jesmin <prtuber70@gmail.com>
2024-07-14 12:29:52 +0800

    Create ইমন.js

---
+++ scripts/commands/ইমন.js
@@ -0,0 +1,61 @@
+const axios = require("axios");
+
+const getApiUrl = async () => {
+  const response = await axios.get('https://raw.githubusercontent.com/sharifvau/Emon-Server/main/emonapi.json');
+  return response.data.emongemini;
+};
+
+module.exports = {
+  config: {
+    name: "ইমন",
+    version: "1.0",
+    credit: "Emon",
+    prefix: 'awto',
+    description: "gemini ai",
+    cooldowns: 5,
+    permission: 0,
+    category: "Google",
+    usages: {
+      en: "{pn} message | photo reply",
+    },
+  },
+  run: async ({ api, args, event }) => {
+    const prompt = args.join(" ");
+    const apiUrl = await getApiUrl();
+
+    if (event.type === "message_reply" && event.messageReply.attachments.length > 0) {
+      const attachmentUrl = event.messageReply.attachments[0].url;
+      try {
+        const response = await axios.get(
+          `${apiUrl}/gemini?q=${encodeURIComponent(attachmentUrl)}&text=${encodeURIComponent(prompt)}`
+        );
+        const message = response.data.response;
+        api.sendMessage(`ইমন RESPONSE:\n ${message}`, event.threadID, event.messageID);
+      } catch (error) {
+        console.error("Error:", error.message);
+        api.sendMessage(`Error: ${error.message}`, event.threadID, event.messageID);
+      }
+    } else if (!prompt) {
+      api.sendMessage(
+        "Please provide a prompt or message reply",
+        event.threadID, 
+        event.messageID
+      );
+    } else {
+      try {
+        const response = await axios.get(
+          `${apiUrl}/mgs?q=${encodeURIComponent(prompt)}`
+        );
+        const message = response.data.generated_text;
+        api.sendMessage(`ইমন RESPONSE:\n ${message}`, event.threadID, event.messageID);
+      } catch (error) {
+        console.error("Error calling Gemini AI:", error.message);
+        api.sendMessage(
+          `Sorry, there was an error processing your request. ${error.message}`,
+          event.threadID,
+          event.messageID
+        );
+      }
+    }
+  },
+};
